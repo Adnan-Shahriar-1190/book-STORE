a54cff37ce3afe084fa8481080c7b19d20a9313a
Adnan-Shahriar-1190 <sb.adnan2020@gmail.com>
2024-06-24 13:34:31 +0600

    backend

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("./conn/conn");
+const userRoutes = require("./routes/user");
+const Books = require("./routes/book");
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", Books);
+
+// Creating port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
