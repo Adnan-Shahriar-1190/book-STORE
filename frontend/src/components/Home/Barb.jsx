8a21262140af2c790f8434aa16da5d2c79368886
zubaermahmud <zubaer.mahmud31@gmail.com>
2024-06-26 00:46:01 +0600

    Footer,Barb

--- frontend/src/components/Home/Barb.jsx
+++ frontend/src/components/Home/Barb.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 
 const Barb = () => {
   return (
-    <div className='h-[75vh]'>
-      <div className="w-3/6">
-      <h1 className='text-6x'>
-
-      </h1>
-      </div>
-      <div className="w-3/6">
-      
+    <div className='h-[80vh] flex flex-col lg:flex-row'>
+      <div className='w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center p-6 lg:p-12 bg-gradient-to-r from-purple-500 to-blue-500'>
+        <h1 className='text-5xl lg:text-7xl font-bold text-white text-center lg:text-left'>
+          Dive Into Your Next Adventure
+        </h1>
+        <p className='mt-6 text-lg lg:text-2xl text-gray-200 text-center lg:text-left'>
+          Explore a world of captivating stories, profound insights, and boundless inspiration in our carefully curated book collection.
+        </p>
+        <div className='mt-10'>
+          <button className='text-white text-lg lg:text-xl font-bold border border-white px-8 py-4 hover:bg-white hover:text-blue-500 rounded-full transition-colors duration-300'>
+            Explore Books
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Barb
+export default Barb;
