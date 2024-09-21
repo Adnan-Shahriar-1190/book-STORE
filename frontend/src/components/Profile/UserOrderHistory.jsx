132b12324897a29d57f5ca969eabbed7f1b1f7b7
zubaermahmud <zubaer.mahmud31@gmail.com>
2024-09-21 23:06:39 +0600

    user order fix

--- frontend/src/components/Profile/UserOrderHistory.jsx
+++ frontend/src/components/Profile/UserOrderHistory.jsx
@@ -35,26 +35,6 @@ const UserOrderHistory = () => {
           <h1 className="text-3xl md:text-5xl font-semibold text-white mb-8">
             Your Order History
           </h1>
-          <div className="mt-4 bg-zinc-800 w-full rounded py-2 px-4 gap-2">
-            <div className="w-[3%]">
-              <h1 className="text-center">Sr.</h1>
-            </div>
-            <div className="w-[22%]">
-              <h1 className="text-center">Books</h1>
-            </div>
-            <div className="w-[45%]">
-              <h1 className="text-center">Description</h1>
-            </div>
-            <div className="w-[9%]">
-              <h1 className="text-center">Price</h1>
-            </div>
-            <div className="w-[16%]">
-              <h1 className="text-center">Status</h1>
-            </div>
-            <div className="w-none md:w-[5%] hidden md:block">
-              <h1 className="">Mode</h1>
-            </div>
-          </div>
           {OrderHistory.map((items,i)=>(
             <div className="bg-zinc-800 w-full rounded py-2 px-4 flex gap-4 hover:bg-zinc-900">
               <div className="w-[3%]">
